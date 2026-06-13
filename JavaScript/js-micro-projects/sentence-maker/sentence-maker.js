52c98aaaaed54089949167f199b39bcb5fc30b26
Sarthak <sarthakbhadouriya@gmail.com>
2026-06-13 22:25:05 +0530

    sentence-maker

---
+++ JavaScript/js-micro-projects/sentence-maker/sentence-maker.js
@@ -0,0 +1,19 @@
+let adjective = "fun";
+let noun = "cat";
+let verb = "jumping";
+let place = "park";
+let adjective2 = "big";
+let noun2 = "fish";
+
+let firstStory = `Once upon a time, there was a ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`First story: ${firstStory}`);
+
+adjective = "cute";
+noun = "dog";
+verb = "running";
+place = "doghouse";
+adjective2 = "small";
+noun2 = "dogfood";
+
+let secondStory = `Once upon a time, there was a ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} ears that flopped when it was ${verb}.`;
+console.log(`Second story: ${secondStory}`);
