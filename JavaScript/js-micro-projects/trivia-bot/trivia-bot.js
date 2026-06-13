70a26e803bb989c4c2901bf358cc91fb7b9ddb29
Sarthak <sarthakbhadouriya@gmail.com>
2026-06-13 22:24:51 +0530

    trivia-bot

---
+++ JavaScript/js-micro-projects/trivia-bot/trivia-bot.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Trivia Bot";
+let botLocation = "The Cloud";
+let favoriteLanguage = "JavaScript";
+
+console.log(`My name is ${botName} and I live on  ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `Did you know that ${favoriteLanguage} was created in just 10 days by Brendan Eich in 1995?`;
+console.log(codingFact);
+
+let codingFact = `Here's another fun fact: ${favoriteLanguage} is used by over 95% of all websites!`;
+console.log(codingFact);
+
+let codingFact = `And one more: ${favoriteLanguage} can be used for both front-end and back-end development!`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}`);
